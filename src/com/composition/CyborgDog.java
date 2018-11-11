97be0ee8473d4be5f98b4fa54e18b6d8a543bf33
Ben Pusey <bpusey@hubspot.com>
2018-11-11 18:51:08 -0500

    Create cyborg dog

---
+++ src/com/composition/CyborgDog.java
@@ -0,0 +1,31 @@
+package com.composition;
+
+public class CyborgDog implements Barkable, BeepBoopable, Poopable {
+  private static double BEEP_BOOP_BATTERY_LIFE_COST = 0.25;
+  private static double BARK_BATTERY_LIFE_COST = 0.5;
+  private Chargeable chargeService;
+  private BeepBoopable beepBoopService;
+  private Barkable barkService;
+  private Poopable poopService;
+
+  CyborgDog(Chargeable chargeService, BeepBoopable beepBoopService, BarkService barkService, Poopable poopService) {
+    this.chargeService = chargeService;
+    this.beepBoopService = beepBoopService;
+    this.barkService = barkService;
+    this.poopService = poopService;
+  }
+
+  public void bark() {
+    this.chargeService.useBatteryLife(BARK_BATTERY_LIFE_COST);
+    this.barkService.bark();
+  }
+
+  public void beepBoop() {
+    this.chargeService.useBatteryLife(BEEP_BOOP_BATTERY_LIFE_COST);
+    this.beepBoopService.beepBoop();
+  }
+
+  public void poop() {
+    this.poopService.poop();
+  }
+}
